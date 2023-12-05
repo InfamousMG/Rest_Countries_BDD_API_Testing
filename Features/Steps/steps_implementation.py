7b6cf15a24184d6b23069e933e62db7601817ccc
Michal Gos <michal.gos@consultant.lego.com>
2023-12-05 14:37:24 +0100

    Create Features folder with steps_implementation.py file and rest_counbtries_API.feature file

---
+++ Features/Steps/steps_implementation.py
@@ -0,0 +1,59 @@
+import requests
+from behave import *
+from Utilities.configuration import get_config
+from Utilities.list_of_countries import countries_list
+
+config = get_config()
+
+
+@given("the name of a {country}")
+def step_given_country_name(context, country):
+    context.country_name = "Poland"
+
+
+@when("I request information about the country via parametrized endpoint")
+def step_when_request_country_info(context):
+    context.url = f"{config['API']['base_url']}/name/{context.country_name}"
+    context.response = requests.get(context.url)
+
+
+@then("the response status code should be 200")
+def step_then_response_status_code(context):
+    assert context.response.status_code == 200
+
+
+@then("the response should contain the same name of the country as in the request")
+def step_then_response_country_info(context):
+    country_info = context.response.json()
+    assert "name" in country_info[0]
+    assert country_info[0]["name"]["common"] == context.country_name
+
+
+@given("that the list of countries should contain 250 countries")
+def step_given_list_of_countries(context):
+    context.complete_list = 250
+
+
+@when("I call /all endpoint")
+def step_when_request_all_endpoint(context):
+    context.countries_list = countries_list()
+
+
+@then("the list of countries has 250 positions")
+def step_then_250_positions(context):
+    assert len(context.countries_list) == context.complete_list
+
+
+@given("the {country} name")
+def step_given_specific_country(context, country):
+    context.specific_country = "Vatican City"
+
+
+@when("I call /all endpoint to create a list of countries")
+def step_when_calling_all_endpoint(context):
+    context.countries_list = countries_list()
+
+
+@then("the <country> is on the list of countries returned by /all endpoint")
+def step_then_country_on_list(context):
+    assert context.specific_country in context.countries_list
---
+++ Features/rest_countries_API.feature
@@ -0,0 +1,19 @@
+# Created by mgos at 30/11/2023
+Feature: Retrieving information about countries
+  # Enter feature description here
+
+  Scenario: Check whether a country is available
+    Given the name of a <country>
+    When I request information about the country via parametrized endpoint
+    Then the response status code should be 200
+    And the response should contain the same name of the country as in the request
+
+  Scenario: Check whether the list of countries is complete
+    Given that the list of countries should contain 250 countries
+    When I call /all endpoint
+    Then the list of countries has 250 positions
+
+  Scenario: Check whether a country is available on the list of countries
+    Given the <country> name
+    When I call /all endpoint to create a list of countries
+    Then the <country> is on the list of countries returned by /all endpoint
