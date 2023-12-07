2c5478ac54617977b4416e4b288af2d5fc50311a
Michal Gos <michal.gos@consultant.lego.com>
2023-12-07 11:14:36 +0100

    Change 2nd scenario to be able to check more than one country

--- Features/Steps/steps_implementation.py
+++ Features/Steps/steps_implementation.py
@@ -19,27 +19,27 @@ def step_when_request_country_info(context):
 
 @then("the response status code should be 200")
 def step_then_response_status_code(context):
-    assert context.response.status_code == 200
+    assert context.response.status_code == 200, "status code is not 200"
 
 
 @then("the response should contain the same name of the country as in the request")
 def step_then_response_country_info(context):
     context.country_info = context.response.json()
-    print(context.country_info)
-    assert "name" in context.country_info[0]
-    assert context.country_info[0]["name"]["common"] == context.country_name
+    assert "name" in context.country_info[0], "name field is not present"
+    assert context.country_info[0]["name"]["common"] == context.country_name, ("the name is not the same as in the "
+                                                                               "request")
 
 
 @then("the response should include essential details like {currencies}, {capital}, {region}, {languages}, {area}, "
       "{population}, and {timezones}")
 def step_then_response_details(context, currencies, capital, region, languages, area, population, timezones):
-    assert "currencies" in context.country_info[0]
-    assert "capital" in context.country_info[0]
-    assert "region" in context.country_info[0]
-    assert "languages" in context.country_info[0]
-    assert "area" in context.country_info[0]
-    assert "population" in context.country_info[0]
-    assert "timezones" in context.country_info[0]
+    assert "currencies" in context.country_info[0], "currencies field is not present"
+    assert "capital" in context.country_info[0], "capital field is not present"
+    assert "region" in context.country_info[0], "region field is not present"
+    assert "languages" in context.country_info[0], "languages field is not present"
+    assert "area" in context.country_info[0], "area field is not present"
+    assert "population" in context.country_info[0], "population field is not present"
+    assert "timezones" in context.country_info[0], "timezones field is not present"
 
 
 @given("that the list of countries should contain 250 countries")
@@ -57,9 +57,9 @@ def step_then_250_positions(context):
     assert len(context.countries_list) == context.complete_list
 
 
-@given("the {country} name")
-def step_given_specific_country(context, country):
-    context.specific_country = "Vatican City"
+@given("the <countries> names in a list")
+def step_given_specific_countries(context):
+    context.specific_countries = ["Vatican City", "Israel", "Bulgaria"]
 
 
 @when("I call /all endpoint to create a list of countries")
@@ -67,6 +67,7 @@ def step_when_calling_all_endpoint(context):
     context.countries_list = countries_list()
 
 
-@then("the <country> is on the list of countries returned by /all endpoint")
-def step_then_country_on_list(context):
-    assert context.specific_country in context.countries_list
+@then("the <countries> is on the list of countries returned by /all endpoint")
+def step_then_countries_on_list(context):
+    assert all(country in context.countries_list for country in context.specific_countries), ("Some of these countries "
+                                                                                              "are not on the list")
--- Features/rest_countries_API.feature
+++ Features/rest_countries_API.feature
@@ -1,8 +1,6 @@
-# Created by mgos at 30/11/2023
 Feature: Retrieving information about countries
-  # Enter feature description here
 
-  Scenario: Check whether a country is available
+  Scenario: Check whether a country is available through country-specific endpoint
     Given the name of a <country>
     When I request information about the country via parametrized endpoint
     Then the response status code should be 200
@@ -14,7 +12,7 @@ Feature: Retrieving information about countries
     When I call /all endpoint
     Then the list of countries has 250 positions
 
-  Scenario: Check whether a country is available on the list of countries
-    Given the <country> name
+  Scenario: Check whether countries in a list are available on the list of countries returned by /all endpoint
+    Given the <countries> names in a list
     When I call /all endpoint to create a list of countries
-    Then the <country> is on the list of countries returned by /all endpoint
+    Then the <countries> is on the list of countries returned by /all endpoint
