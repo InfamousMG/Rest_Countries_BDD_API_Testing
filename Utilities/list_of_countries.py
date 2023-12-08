ce5112f02c0b7c4c2945dd53567675562706f309
Michal Gos <michal.gos@consultant.lego.com>
2023-12-08 08:47:28 +0100

    Refactor countries_list function and add countries_count function

--- Utilities/list_of_countries.py
+++ Utilities/list_of_countries.py
@@ -4,11 +4,16 @@ response = requests.get(url="https://restcountries.com/v3.1/all")
 response_json = response.json()
 
 
+def countries_count():
+    number_of_countries = len(response_json)
+    return number_of_countries
+
+
 def countries_list():
     list_of_countries = []
-    number_of_country = 0
-    for country in range(0, 250):
-        list_of_countries.append(response_json[number_of_country]["name"]["common"])
-        number_of_country += 1
+    country_position = 0
+    for country in range(0, countries_count()):
+        list_of_countries.append(response_json[country_position]["name"]["common"])
+        country_position += 1
 
     return list_of_countries
